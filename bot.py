3259fdc7a174942896aa27fd9a2f2f9414f0b418
Yousuf El Baesy <yousuf5550@gmail.com>
2020-07-29 21:17:40 +0400

    OMG!

--- bot.py
+++ bot.py
@@ -2,6 +2,16 @@ from selenium import webdriver
 
 import os
 import time
+import json
+
 
 class InstagramBot:
     def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+
+if __name__ == '__main__':
+    with open('./config.json') as f:
+        data = json.load(f)
+    ig_bot = InstagramBot(data["username"], data["password"])
